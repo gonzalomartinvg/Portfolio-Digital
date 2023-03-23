3f92cadb5584ecd6c4e2d548127f566cf0aa2b93
Gonzalo <gonzalomartinvg@gmail.com>
2023-03-23 11:50:47 -0300

    commit nueva pagina

---
+++ js.js
@@ -0,0 +1,356 @@
+//OBJETO
+
+const proyectosweb = {
+    
+    "fotball-simulator":[
+        {
+            "nombre": "Football Simulator",
+
+            "tipo": "Desarrollo Web y Móvil",
+
+            "imagen": "img/tarjetas/football-simulator.webp",    
+
+            "urlweb": "https://gonzalomartinvg.github.io/football-simulator-2022/",
+
+            "urlrepositorio": "https://github.com/gonzalomartinvg/football-simulator-2022"
+        }
+    ],
+
+    "tienda-online-casla":[
+        {
+            "nombre": "Tienda online CASLA",
+
+            "tipo": "Desarrollo Web y Móvil",
+
+            "imagen": "img/tarjetas/sanlo.webp",    
+
+            "urlweb": "https://gonzalomartinvg.github.io/tienda-san-lorenzo/",
+
+            "urlrepositorio": "https://github.com/gonzalomartinvg/tienda-san-lorenzo"
+        }
+    ],
+
+    "mop-bank":[
+        {
+            "nombre": "Mop Bank",
+
+            "tipo": "Desarrollo Web y Móvil",
+
+            "imagen": "img/tarjetas/mop-bank.webp",    
+
+            "urlweb": "https://gonzalomartinvg.github.io/mop-bank/",
+
+            "urlrepositorio": "https://github.com/gonzalomartinvg/mop-bank"
+        }
+    ],
+
+    "corcho":[
+        {
+            "nombre": "Corcho",
+
+            "tipo": "Desarrollo Web y Móvil",
+
+            "imagen": "img/tarjetas/corchoweb.webp",    
+
+            "urlweb": "https://www.corchobebidas.com/",
+
+            "urlrepositorio": "https://github.com/gonzalomartinvg/corchoweb"
+        }
+    ],
+
+    "gonza-viaja":[
+        {
+            "nombre": "Gonza Viaja",
+
+            "tipo": "Desarrollo Web y Móvil",
+
+            "imagen": "img/tarjetas/gonzaviaja.webp",    
+
+            "urlweb": "https://gonzaviaja.com/",
+
+            "urlrepositorio": "https://github.com/gonzalomartinvg/gonza-viaja"
+        }
+    ],
+
+    "a-los-pedazos":[
+        {
+            "nombre": "ALPA digital",
+
+            "tipo": "Desarrollo Web",
+
+            "imagen": "img/tarjetas/a-los-pedazos.webp",    
+
+            "urlweb": "https://www.alpagenciadigital.com/",
+
+            "urlrepositorio": "https://github.com/gonzalomartinvg/A-Los-Pedazos-Agencia-Digitial"
+        }
+    ],
+
+    "github":[
+        {
+            "nombre": "Github",
+
+            "tipo": "Desarrollo Web y Móvil",
+
+            "imagen": "img/tarjetas/github.webp",    
+
+            "urlweb": "https://github.com/gonzalomartinvg",
+
+            "urlrepositorio": ""
+        }
+    ]
+
+}
+
+const proyectosaudiovisuales = {
+    
+    "napoles":[
+        {
+            "nombre": "Nápoles",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/napoles.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/napoles",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "corcho":[
+        {
+            "nombre": "Corcho",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/corcho.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/corcho",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "tapiocas-portunhol":[
+        {
+            "nombre": "Tapiocas Portunhol",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/portunhol.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/portunhol",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "gebers":[
+        {
+            "nombre": "Geber´s",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/gebers.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/gebers",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "anmac":[
+        {
+            "nombre": "ANMaC",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/anmac.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/anmac",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "destino-playas":[
+        {
+            "nombre": "Destino Playas",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/destino-playas.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/destino-playas",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "desarme-voluntario":[
+        {
+            "nombre": "Desarme voluntario",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/desarme-voluntario.webp",    
+
+            "urlweb": "img/tarjetas/desarme-voluntario.webp",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "proyectos-personales":[
+        {
+            "nombre": "Proyectos personales",
+
+            "tipo": "Producción audiovisual",
+
+            "imagen": "img/tarjetas/proyectos-personales.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/proyectos-personales",
+
+            "urlrepositorio": "",
+        }
+    ], 
+}
+
+const proyectosdisenouxui = {
+    
+    "meeteam":[
+        {
+            "nombre": "Meeteam App",
+
+            "tipo": "Diseño UX/UI",
+
+            "imagen": "img/tarjetas/meeteam.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/meeteam-app",
+
+            "urlrepositorio": "",
+        }
+    ],
+
+    "mundo-digital":[
+        {
+            "nombre": "Mundo Digital",
+
+            "tipo": "Diseño UX/UI",
+
+            "imagen": "img/tarjetas/mundo-digital.webp",    
+
+            "urlweb": "https://gonzalomartinvg.wixsite.com/portfolio/mundo-digital",
+
+            "urlrepositorio": "",
+        }
+    ]
+}
+
+//SECCIÓN 2: TARJETAS
+
+//Función mostrar tarjetas
+
+function mostradorTarjetas(a, b){
+
+    let nombreProyecto = ""
+    let tipoProyecto = ""
+    let imagenProyecto = ""
+    let urlWebProyecto = ""
+    let urlRepositorioProyecto = ""
+    let todosLoscontenedoresTarjetas = document.querySelectorAll(".contenedor-tarjetas"); //Usamos un selector ALL, porque en la home hay más de un contenedor con la clase ".contenedor-relacionados"
+
+    todosLoscontenedoresTarjetas.forEach(element => { //Con el foreach recorremos el array que surge del querySelectorAll y a cada uno le indicamos que ejecute la función según el "identificadorpais2"
+
+        if (element.dataset.identificadortipo == b){ //Con este condicional evitamos que aparezca en el listado los botones de destinos de otros países.
+
+            for (prop in a){
+                
+                        nombreProyecto = (a[prop][0].nombre);
+                        tipoProyecto = (a[prop][0].tipo);
+                        imagenProyecto = (a[prop][0].imagen);
+                        urlWebProyecto = (a[prop][0].urlweb);
+                        urlRepositorioProyecto = (a[prop][0].urlrepositorio);
+
+                        let htmlRepositorio = ""
+
+                        //Hacemos un condicional para chequear si le proyecto tiene repositorio o no. Si el repositorio no está vacío, genera un botón que linkea al repositorio y en tipo de link lo de fine como "web". En cambio, si no tiene repositorio, el tipo de link lo indica como "Más info" (corresponde a proyectos audiovisuales y de diseño ux/ui)
+
+                        if (urlRepositorioProyecto != ""){
+                            htmlRepositorio = `<a class="boton-tarjeta repo" href="${urlRepositorioProyecto}">Repositorio</a>`
+                            tipoDeLink = "Web";
+                        }
+
+                        if (urlRepositorioProyecto == ""){
+                            tipoDeLink = "Más info";
+                        }
+            
+                        const divAutogenerado = document.createElement("div");
+                        divAutogenerado.classList.add("contenedor-tarjeta-individual")
+                        const contenidoAutoGenerado = //Las `` siguientes se utilizan para hacer un html literal
+                            `
+                            <img class="imagen-proyecto" src=${imagenProyecto} alt="Foto de proyecto"/>
+                            <p class="nombre-proyecto">${nombreProyecto}</p>
+                            <p class="tipo-de-proyecto">${tipoProyecto}</p>
+
+                            <div class="contenedor-botones">
+
+                                <a class="boton-tarjeta mas-info" href="${urlWebProyecto}">${tipoDeLink}</a>
+                                ${htmlRepositorio}
+                
+                            </div>
+                            
+                            `; 
+            
+                        divAutogenerado.innerHTML = contenidoAutoGenerado;
+            
+                        element.append(divAutogenerado);
+    
+                    }
+        }
+    });
+
+}
+
+//Ejecución de funciones
+
+mostradorTarjetas(proyectosweb, "proyectosweb");
+mostradorTarjetas(proyectosaudiovisuales, "proyectosaudiovisuales");
+mostradorTarjetas(proyectosdisenouxui, "proyectosdisenouxui");
+
+//Movimiento scroll del apartado de tarjetas
+
+const contenedorTarjetas = document.querySelectorAll('.contenedor-tarjetas') //Seleccionamos todos los contenedores de tarjetas, el de desarrollo web, proyectos audiovisules y diseño ux/ui.
+
+const contenedorImagen = document.querySelectorAll('.imagen-proyecto')
+
+contenedorTarjetas.forEach(element => { //Con el foreach recorremos el array que surge del querySelectorAll y a cada uno le indicamos que ejecute la función según el "identificadorpais"
+
+        let pressed = false
+        let startX = 0
+        
+        element.addEventListener('mousedown', function (e) {
+          pressed = true
+          startX = e.clientX
+          this.style.cursor = 'grabbing'
+        })
+        
+        element.addEventListener('mouseleave', function (e) {
+          pressed = false
+        })
+        
+        window.addEventListener('mouseup', function (e) {
+          pressed = false
+          element.style.cursor = 'grab'
+        })
+        
+        element.addEventListener('mousemove', function (e) {
+          if(!pressed) {
+            return
+          }
+        
+          this.scrollLeft += startX - e.clientX
+        })
+
+});
